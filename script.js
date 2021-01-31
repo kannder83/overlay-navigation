96a264a8710407a11c74c59972d289f5cef2e008
Alejandro Ruiz <kannder.ziur@gmail.com>
2021-01-30 22:36:50 -0500

    I complete the js script.

---
+++ script.js
@@ -0,0 +1,14 @@
+function navigation() {
+  const icon = document.getElementById("icon");
+  const navbar = document.getElementById("navbar");
+
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+    navbar.style.left = "0";
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+    navbar.style.left = "-100%";
+  }
+}
